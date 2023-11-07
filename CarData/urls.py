2b9aa1230425005c282979653703d6bebab0a506
Suraj Rajbanshi <soorajrajbanshi@cagtu.com>
2023-11-07 09:46:01 +0545

    serializers

--- CarData/urls.py
+++ CarData/urls.py
@@ -3,5 +3,7 @@ from .import views
 
 urlpatterns = [
     path('', views.carlist, name='car_list'),
+    path('add/', views.add_car, name='addcar'),
     path('detail/<id>/', views.cardetail, name='detail'),
+    path('car/edit/<id>/', views.car_detail_edit, name="edit"),
 ]
--- CarData/views.py
+++ CarData/views.py
@@ -1,20 +1,74 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .api_file.serializers import CarSerializers
 from .models import Carlist
 # Create your views here.
 
+# def carlist(request):
+#     car = Carlist.objects.all()
+#     data = {
+#         'car':list(car.values()),
+#     }
+
+#     return JsonResponse(data)
+
+
+# def cardetail(request, id):
+#     car = Carlist.objects.get(id=id)
+#     data = {
+#         'name' : car.name,
+#         'description' : car.description,
+#         'status' : car.active
+#     }
+#     return JsonResponse(data)
+
+
+@api_view(["GET"])
 def carlist(request):
     car = Carlist.objects.all()
-    data = {
-        'car' : list(car.values()),
-    }
-    return JsonResponse(data)
 
+    serializer = CarSerializers(car,many=True)
+    return Response(serializer.data)
+
+@api_view(['GET','POST'])
+def add_car(request):
+    if request.method == "GET":
+        car = Carlist.objects.all()
+        serializer = CarSerializers(car,many=True)
+        return Response(serializer.data)
+    
+    if request.method == "POST":
+        serializer = CarSerializers(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+
+@api_view(['GET'])
 def cardetail(request, id):
     car = Carlist.objects.get(id=id)
-    data = {
-        'name' : car.name,
-        'description' : car.description,
-        'status' : car.active
-    }
-    return JsonResponse(data)
+    serializer = CarSerializers(car)
+
+    return Response(serializer.data)
+
+
+@api_view(['GET','PUT'])
+def car_detail_edit(request, id):
+    if request.method == "GET":
+        car = Carlist.objects.get(id=id)
+        serializer = CarSerializers(car)
+        return Response(serializer.data)
+
+
+    if request.method == "PUT":
+        car = Carlist.objects.get(id=id)
+        serializer = CarSerializers(car, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.error)
